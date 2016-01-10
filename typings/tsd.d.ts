09423a106a126459fc7f76ba53d01226796f7de3
Genadijus Paleckis <genadijus.paleckis@gamesys.co.uk>
2016-01-10 01:07:26 +0000

    add external types

---
+++ typings/tsd.d.ts
@@ -0,0 +1,7 @@
+
+/// <reference path="express/express.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="socket.io/socket.io.d.ts" />
+/// <reference path="mime/mime.d.ts" />
+/// <reference path="serve-static/serve-static.d.ts" />
+/// <reference path="lodash/lodash.d.ts" />
