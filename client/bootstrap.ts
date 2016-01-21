20fee3aa502c4d8bced71d5d48077c3b209e9115
Genadijus Paleckis <genadijus.paleckis@gamesys.co.uk>
2016-01-21 21:24:09 +0000

    enable conditional debug

--- client/bootstrap.ts
+++ client/bootstrap.ts
@@ -1,4 +1,16 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {App} from './components/app/app';
 
+var debug = false;
+
+if (debug) {
+  localStorage.debug = '*';
+} else if (localStorage.debug) {
+  delete localStorage.debug;
+}
+
 bootstrap(App);
+
+declare var localStorage: {
+  debug: boolean | string;
+}
