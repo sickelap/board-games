3babd1f39be23b956097e061a44c7e7cc4ef8f23
Genadijus Paleckis <genadijus.paleckis@gamesys.co.uk>
2016-01-23 23:00:59 +0000

    add profile

---
+++ client/components/profile/profile.component.ts
@@ -0,0 +1,22 @@
+import {Component} from 'angular2/core'
+import {FormBuilder, Validators} from 'angular2/common'
+import {Router} from 'angular2/router';
+
+@Component({
+  selector: 'profile',
+  templateUrl: 'components/profile/profile.component.html'
+})
+export class ProfileComponent {
+  private profileForm;
+
+  constructor(private router: Router, private fb: FormBuilder) {
+    this.profileForm = fb.group({
+      username: [localStorage.getItem('username'), Validators.required]
+    });
+  }
+  save(ev) {
+    ev.preventDefault();
+    localStorage.setItem('username', this.profileForm.controls.username.value);
+    this.router.parent.navigate(['Lobby']);
+  }
+}
